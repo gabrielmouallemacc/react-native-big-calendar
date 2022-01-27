eef2d0c5142cf5e5330894d7d1dcb5d37b546aa4
Gabriel Mouallem <gabriel.j.rodrigues@accenture.com>
2022-01-27 11:57:50 -0300

    Rollback panResponder hook that was comented

--- src/hooks/usePanResponder.ts
+++ src/hooks/usePanResponder.ts
@@ -12,34 +12,34 @@ export function usePanResponder({
 }) {
   const [panHandled, setPanHandled] = React.useState(false)
 
-  // const panResponder = React.useMemo(
-  //   () =>
-  //     PanResponder.create({
-  //       // see https://stackoverflow.com/questions/47568850/touchableopacity-with-parent-panresponder
-  //       onMoveShouldSetPanResponder: (_, { dx, dy }) => {
-  //         return dx > 2 || dx < -2 || dy > 2 || dy < -2
-  //       },
-  //       onPanResponderMove: (_, { dy, dx }) => {
-  //         if (dy < -1 * SWIPE_THRESHOLD || SWIPE_THRESHOLD < dy || panHandled) {
-  //           return
-  //         }
-  //         if (dx < -1 * SWIPE_THRESHOLD) {
-  //           onSwipeHorizontal && onSwipeHorizontal('LEFT')
-  //           setPanHandled(true)
-  //           return
-  //         }
-  //         if (dx > SWIPE_THRESHOLD) {
-  //           onSwipeHorizontal && onSwipeHorizontal('RIGHT')
-  //           setPanHandled(true)
-  //           return
-  //         }
-  //       },
-  //       onPanResponderEnd: () => {
-  //         setPanHandled(false)
-  //       },
-  //     }),
-  //   [panHandled, onSwipeHorizontal],
-  // )
+  const panResponder = React.useMemo(
+    () =>
+      PanResponder.create({
+        // see https://stackoverflow.com/questions/47568850/touchableopacity-with-parent-panresponder
+        onMoveShouldSetPanResponder: (_, { dx, dy }) => {
+          return dx > 2 || dx < -2 || dy > 2 || dy < -2
+        },
+        onPanResponderMove: (_, { dy, dx }) => {
+          if (dy < -1 * SWIPE_THRESHOLD || SWIPE_THRESHOLD < dy || panHandled) {
+            return
+          }
+          if (dx < -1 * SWIPE_THRESHOLD) {
+            onSwipeHorizontal && onSwipeHorizontal('LEFT')
+            setPanHandled(true)
+            return
+          }
+          if (dx > SWIPE_THRESHOLD) {
+            onSwipeHorizontal && onSwipeHorizontal('RIGHT')
+            setPanHandled(true)
+            return
+          }
+        },
+        onPanResponderEnd: () => {
+          setPanHandled(false)
+        },
+      }),
+    [panHandled, onSwipeHorizontal],
+  )
 
-  return { panHandlers: {} }
+  return panResponder
 }
